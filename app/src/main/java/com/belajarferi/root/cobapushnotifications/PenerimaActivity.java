3291236717f8ff3916524772fd87eb36fdfc3aea
feriagusirawan <feriagusirawan02@gmail.com>
2018-12-07 16:11:15 +0700

    Initial commit

---
+++ app/src/main/java/com/belajarferi/root/cobapushnotifications/PenerimaActivity.java
@@ -0,0 +1,12 @@
+package com.belajarferi.root.cobapushnotifications;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PenerimaActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_penerima);
+    }
+}
